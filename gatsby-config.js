3374b220d0d1172cb9b7e3cd619cbc546f1dfade
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2020-05-11 17:47:22 -0300

    Adds gtag manager plugin and configures ga.
    Signed-off-by: Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,13 +78,37 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID UA-110599884-1",
+        trackingId: "UA-110599884-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,        
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-110599884-2", // Google Analytics / GA          
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {          
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     {
