db2ae72208005f38bb2fe9e54dbe5f0b249dfb92
James Cole <james@openstack.org>
2021-04-17 18:45:34 -0500

    Add functional video
    Signed-Off By: <james@openstack.org>

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -16,14 +16,18 @@ const VideoSection = class extends React.Component {
            <div className="video-section">
             <div className="container">
                 <div className="section-content">
-                    <div className="video"></div>
+                    <div className="video">
+                        <div className="videoWrapper">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/gkcQrTp5Kks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
                     <div className="description">
-                    <h3 className="article-small__title news-title">Latest News</h3>
-                    <h4 className="article-small__entry">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere libero vitae molestie laoreet. Praesent tempor varius felis, vitae faucibus elit commodo eu. Quisque sapien nulla, sollicitudin nec felis quis, porta tempor libero. Donec et nisl id erat egestas molestie gravida et ante. Nulla ut cursus nunc. Maecenas eu metus ullamcorper, pharetra est porta, imperdiet metus.</h4>
+                        <h3 className="article-small__title news-title">Latest News</h3>
+                        <h4 className="article-small__entry">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere libero vitae molestie laoreet. Praesent tempor varius felis, vitae faucibus elit commodo eu. Quisque sapien nulla, sollicitudin nec felis quis, porta tempor libero. Donec et nisl id erat egestas molestie gravida et ante. Nulla ut cursus nunc. Maecenas eu metus ullamcorper, pharetra est porta, imperdiet metus.</h4>
 
-                    <h4>
-                    <a href="/community"><strong>Call to action goes here.</strong></a>
-                    </h4>
+                        <h4>
+                        <a href="/community"><strong>Call to action goes here.</strong></a>
+                        </h4>
 
                     </div>
                 </div>
