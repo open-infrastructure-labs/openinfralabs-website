b101f12b2fb22e760744cbcad4ae71763e3737e2
James Cole <james@openstack.org>
2021-04-22 11:03:32 -0500

    Add video caption
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/HeaderVideo.js
+++ src/components/HeaderVideo.js
@@ -18,25 +18,27 @@ const HeaderVideo = class extends React.Component {
               <div className="hero-video-content">
 
               <div className="hero-video-text">
+              
                 <h1 className="hero-title">Announcing the first OpenInfra Labs region</h1>
                 <h4 className="hero-video-subhead">
                 Thanks in part to the generosity of Red Hat, the new open research cloud initiative has been launched at Boston University to advance the future of open infrastructure.
                 </h4>
-                <div className="hero-actions">
-                      <a href="" className="button is-primary">
-                        <span>BUTTON TEXT</span> <span className="ico">
-                          <img src={leftArrow} alt="Learn More" style={({ filter: 'invert(36%) sepia(45%) saturate(953%) hue-rotate(170deg) brightness(98%) contrast(86%)' }, { WebkitFilter: 'invert(36%) sepia(45%) saturate(953%) hue-rotate(170deg) brightness(98%) contrast(86%)' })} />
-                        </span>
-                      </a>
-                </div>
+
+                  <div className="hero-actions">
+                    <a href="" className="button is-primary">
+                    <span>GET INVOLVED</span> <span className="ico">
+                      <img src={leftArrow} alt="Learn More" style={({ filter: 'invert(36%) sepia(45%) saturate(953%) hue-rotate(170deg) brightness(98%) contrast(86%)' }, { WebkitFilter: 'invert(36%) sepia(45%) saturate(953%) hue-rotate(170deg) brightness(98%) contrast(86%)' })} />
+                      </span>
+                    </a>
+                  </div>
                 </div>
         
                 <div className="video-hero">
-                        <div className="videoWrapper">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/gkcQrTp5Kks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div>
-
+                  <div className="videoWrapper">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/gkcQrTp5Kks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                  <div className="video-caption">Watch the announcement</div>
+                </div>
               </div>
             </div>
           </div>
