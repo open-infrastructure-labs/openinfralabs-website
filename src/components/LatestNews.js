d627872e6774cce0ba53b0d30befc6d4ef16f3b9
James Cole <james@openstack.org>
2021-04-26 14:43:44 -0500

    Add More About the Announcement
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -18,11 +18,11 @@ const LatestNews = class extends React.Component {
                 <div className="section-content">
 
                     <div className="latest-news-description">
-                        <h3 className="article-small__title news-title">Latest News</h3>
-                        <h4 className="article-small__entry">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere libero vitae molestie laoreet. Praesent tempor varius felis, vitae faucibus elit commodo eu. Quisque sapien nulla, sollicitudin nec felis quis, porta tempor libero. Donec et nisl id erat egestas molestie gravida et ante. Nulla ut cursus nunc. Maecenas eu metus ullamcorper, pharetra est porta, imperdiet metus.</h4>
+                        <h3 className="article-small__title news-title">More About the Announcement</h3>
+                        <h4 className="article-small__entry">OpenInfra Foundation Platinum Member <a className="news-link" href="https://www.redhat.com/en/about/press-releases/red-hat-and-boston-university-announce-major-partnership-advance-open-hybrid-cloud-research-and-operations-scale">Red Hat is providing $551.9 million US</a> in software subscriptions and resources to the Hariri Institute’s open research cloud initiative, in addition to $20 million to support research and deepen collaboration under the Red Hat Collaboratory at Boston University. This effort is creating a production research cloud that serves as the first, founding region in the OpenInfra Labs Cloud Network, which is envisioned to be a network of production clouds connecting open source projects to production. OpenInfra Labs will enable the cloud services and network deployed in this founding region to be replicated to other regions, federated between these regions and extended beyond academia.</h4>
 
                         <h4>
-                        <a href="/community"><strong>Call to action goes here.</strong></a>
+                        <a href="https://openinfrafoundation.formstack.com/forms/openinfra_labs_interest"><strong>Support OpenInfra Labs</strong></a>
                         </h4>
 
                     </div>
